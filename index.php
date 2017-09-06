ec6e159b6a4fcf53e0dfe9b0026a93a31e4c4fd9
Frida Melin <fm222gn@student.lnu.se>
2017-09-06 13:04:07 +0200

    ändrade filerna

--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo("<html><button type='button'>Logga in!</button><button type='button'>Skapa användare!</button><br></html>");
-
-echo("<html><br>Användarnamn:<input type='text'><br>Lösenord:<input type='text'></html>");
